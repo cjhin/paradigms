0afcac00f25f80eb93b42daa69aefc0fdd876cdb
Ryan Shea <rshea@nd.edu>
2013-05-03 20:51:07 -0400

    made adding modules dynamic

--- modules/ls.py
+++ modules/ls.py
@@ -4,7 +4,8 @@ class module_ls:
     def index(self): 
         proc = subprocess.Popen("ls", stdout=subprocess.PIPE) 
         output = proc.stdout.read() 
-        return ''' 
-            <p>The output of ls is</p>'''+output+''' 
-        ''' 
+        return '''     
+                      <h1>ls</h1>
+                      <p>'''+output+'''</p> 
+               ''' 
     index.exposed = True
