c76a04b94d3e92ec9eec4087ee34fbff93ed5936
Chas Jhin <csjhin@gmail.com>
2013-05-04 23:28:30 -0400

    added styling to login page

--- final_proj.py
+++ final_proj.py
@@ -52,12 +52,46 @@ class homepage:
 
     #at this point no cookie was found so print out authentication
     HTML= '''
+      <style type="text/css">
+		  body {
+			padding-top: 40px;
+			padding-bottom: 40px;
+			background-color: #f5f5f5;
+		  }
+
+		  .form-signin {
+			max-width: 300px;
+			padding: 19px 29px 29px;
+			margin: 0 auto 20px;
+			background-color: #fff;
+			border: 1px solid #e5e5e5;
+			-webkit-border-radius: 5px;
+			   -moz-border-radius: 5px;
+					border-radius: 5px;
+			-webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+			   -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+					box-shadow: 0 1px 2px rgba(0,0,0,.05);
+		  }
+		  .form-signin .form-signin-heading,
+		  .form-signin .checkbox {
+			margin-bottom: 10px;
+		  }
+		  .form-signin input[type="text"],
+		  .form-signin input[type="password"] {
+			font-size: 16px;
+			height: auto;
+			margin-bottom: 15px;
+			padding: 7px 9px;
+		  }	
+	  </style>
+	  <body>
 	  <form class="form-signin" action="signin">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input name="info" type="text" class="input-block-level" placeholder="Username">
         <input name="info" type="password" class="input-block-level" placeholder="Password">
-        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
-      </form>'''
+        <button class="btn" type="submit">Sign in</button>
+      </form>
+      </body>'''
     return HTML
   index.exposed = True
 
